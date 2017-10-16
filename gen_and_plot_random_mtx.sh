69dffef4ff03464848ff0ba226113c532d3c5305
Eric Bridgeford <ebridge2@jhu.edu>
2017-10-16 11:41:13 -0400

    added skeleton shell code

---
+++ gen_and_plot_random_mtx.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# a bare-bones wrapper for a python script to generate and plot a random matrix.
+#
+# Inputs
+#    $1: the number of features
+#    $2: the number of observations
+#    $3: the path to the output image
+# Usage
+#    ./gen_and_plot_random_mtx.sh <n> <t> </path/to/output.png
+#
+
+# YOUR CODE HERE
